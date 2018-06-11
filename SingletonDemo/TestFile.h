a1c8169ce7f15459ced0c677e127bc13df41f235
TianBaoLi <13143337451@163.com>
2018-06-11 16:24:28 +0800

    test

---
+++ SingletonDemo/TestFile.h
@@ -0,0 +1,13 @@
+//
+//  TestFile.h
+//  SingletonDemo
+//
+//  Created by li_tianbao on 2018/6/11.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestFile : UIView
+
+@end
---
+++ SingletonDemo/TestFile.m
@@ -0,0 +1,21 @@
+//
+//  TestFile.m
+//  SingletonDemo
+//
+//  Created by li_tianbao on 2018/6/11.
+//  Copyright © 2018年 Lee. All rights reserved.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
